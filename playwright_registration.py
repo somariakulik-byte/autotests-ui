28dab7eeef7dec61d5b4b6eadfae1ca1ac5ef8fe
somariakulik-byte <somariakulik@gmail.com>
2026-05-13 15:20:16 +0300

    Работа с авторизацией в Playwright.Практика работы с авторизацией в Playwright

--- playwright_registration.py
+++ playwright_registration.py
@@ -1,24 +1,35 @@
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import sync_playwright
 
 with sync_playwright() as playwright:
-    # Открываем браузер и создаем новую страницу
+    # Регистрация
     browser = playwright.chromium.launch(headless=False)
-    page = browser.new_page()
+    context = browser.new_context()
+    page = context.new_page()
 
-    # Переходим на страницу входа
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
     email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-    email_input.fill("user.name@gmail.com")
+    email_input.fill('user.name@gmail.com')
 
     username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-    username_input.fill("username")
+    username_input.fill('username')
 
     password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-    password_input.fill("password")
+    password_input.fill('password')
 
     registration_button = page.get_by_test_id('registration-page-registration-button')
     registration_button.click()
 
-    dashboard_word = page.get_by_test_id('dashboard-toolbar-title-text')
-    expect(dashboard_word).to_be_visible()
+    # Ожидаем редирект или появление элемента на дашборде
+    page.wait_for_url("**/dashboard")
+
+    # Сохраняем состояние
+    context.storage_state(path="browser-state.json")
+
+    # Переиспользуем ту же страницу или создаем новую
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
+
+    # Проверяем, что мы на дашборде
+    assert page.url.endswith("/dashboard")
+
+    page.wait_for_timeout(5000)
